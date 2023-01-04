89eb3f5df235145494ed1b752ce7cb29368d259b
Sushant Seth <sushant@Sushants-MacBook-Air.local>
2023-01-04 23:50:16 +0530

    parcel introduction

---
+++ app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+    let heading = React.createElement("h1",{},"namaste react")
+    let heading2 = React.createElement("h1",{},"namaste react2")
+    let container = React.createElement("div",{
+        id:"container"
+    }, [heading,heading2])
+    let root = ReactDOM.createRoot(document.getElementById("root"))
+    root.render(container)
